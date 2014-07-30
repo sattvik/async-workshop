6df67b82fc2114a82748aabe2821ef23a620be6a
Daniel Solano Gómez <daniel@solanogomez.org>
2014-07-30 08:04:11 -0500

    Refactor page generation

--- src/clj/async_workshop/chat_demo/pages.clj
+++ src/clj/async_workshop/chat_demo/pages.clj
@@ -1,44 +1,27 @@
 (ns async-workshop.chat-demo.pages
-  (:require [async-workshop.server.pages :refer [workshop-page]]
+  (:require [async-workshop.server.workshop-page :refer [defpage]]
             [cemerick.friend :as friend]
-            [net.cgrand.enlive-html :as el :refer [defsnippet]]))
+            [net.cgrand.enlive-html :as el]))
 
-(defsnippet landing-page-content "templates/chat/landing-page.html"
-  [:template :> el/any-node]
-  [_])
+(defpage landing-page
+  "templates/chat/landing-page.html"
+  []
+  {:title "Chat demo"
+   :subtitle "Chat with core.async"})
 
-(defn landing-page
+(defpage login-page
+  "templates/chat/login-page.html"
+  []
+  {:title "Chat demo"
+   :subtitle "Login"})
+
+(defpage chatroom
+  "templates/chat/chatroom.html"
+  []
+  {:title "Chat room"
+   :compact? true}
   [req]
-  (workshop-page
-    req
-    {:title "Chat demo"
-     :subtitle "Chat with core.async"
-     :content (landing-page-content req)}))
-
-(defsnippet login-page-content "templates/chat/login-page.html"
-  [:template :> el/any-node]
-  [_])
-
-(defn login-page
-  [req]
-  (workshop-page
-    req
-    {:title "Chat demo"
-     :subtitle "Login"
-     :content (login-page-content req)}))
-
-(defsnippet chatroom-content "templates/chat/chatroom.html"
-  [:template :> el/any-node]
-  [{:keys [chatname]}]
-  [:h2] (el/content "Welcome to the chat, " chatname))
-
-(defn chatroom
-  [req]
-  (workshop-page
-    req
-    {:tatle "Chat room"
-     :compact? true
-     :content (chatroom-content
-                (assoc req :chatname (get-in req [:session
-                                                  ::friend/identity
-                                                  :current])))}))
+  [:h2] (el/content "Welcome to the chat, "
+                    (get-in req [:session
+                                 ::friend/identity
+                                 :current])))
