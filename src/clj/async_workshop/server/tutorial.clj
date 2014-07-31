ef3e1b042afcc2831e7aef62d7ab59917f95fae3
Daniel Solano Gómez <daniel@solanogomez.org>
2014-07-30 21:42:00 -0500

    Syntax highlighting/licensing update
    - Add licensing information
    - Use SyntaxHighlighter
    - Fix rendering of API invocations

--- src/clj/async_workshop/server/tutorial.clj
+++ src/clj/async_workshop/server/tutorial.clj
@@ -1,3 +1,12 @@
+;
+; Copyright © 2014 Daniel Solano Gómez.
+;
+; This program is provided under the terms of the Eclipse Public License 1.0
+; <http://www.eclipse.org/org/documents/epl-v10.html>.  Any use, reproduction,
+; or distribution of this program constitutes recipient's acceptance of this
+; license.
+;
+
 (ns async-workshop.server.tutorial
   (:require [async-workshop.server.workshop-page :refer [defpage]]
             [compojure.core :refer [defroutes GET]]))
