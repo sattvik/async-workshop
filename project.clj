9d1552d877566512263bcdfa0ab4703ca70eb5c9
Daniel Solano Gómez <daniel@solanogomez.org>
2014-08-01 16:33:23 -0500

    Update dependencies
    - om 0.6.4 -> 0.7.0
    - clojurescript 0.0-2268 -> 0.0-2277
    - react 0.9.0 -> 0.11.1

--- project.clj
+++ project.clj
@@ -4,14 +4,14 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2268"]
+                 [org.clojure/clojurescript "0.0-2277"]
                  [org.clojure/core.async "0.1.303.0-886421-alpha"]
                  [com.cemerick/friend "0.2.1" :exclusions [org.clojure/core.cache]]
                  [compojure "1.1.8"]
                  [enlive "1.1.5"]
                  [http-kit "2.1.16"]
                  [javax.servlet/servlet-api "2.5"]
-                 [om "0.6.4"]
+                 [om "0.7.0"]
                  [ring/ring-core "1.3.0"]]
   :plugins [[lein-cljsbuild "1.0.3"]]
   :source-paths ["src/clj" "src/chat-demo" "src/channel-demo"]
